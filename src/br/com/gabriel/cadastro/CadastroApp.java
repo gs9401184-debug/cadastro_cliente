71ad40bd8a3ca67014dd9f220c15da5047297b7f
GABRIEL SOUSA RODRIGUES DE MELO <25203630@CFP127ALUNO.LOCAL>
2025-11-24 08:11:32 -0300

    Iniciando o Projeto

--- src/br/com/gabriel/cadastro/CadastroApp.java
+++ src/br/com/gabriel/cadastro/CadastroApp.java
@@ -1,4 +1,7 @@
 package br.com.gabriel.cadastro;
 
 public class CadastroApp {
+    public static void main(String[] args) {
+        System.out.println("Cadastro App");
+    }
 }
--- src/br/com/gabriel/cadastro/model/Cliente.java
+++ src/br/com/gabriel/cadastro/model/Cliente.java
@@ -1,4 +1,19 @@
 package br.com.gabriel.cadastro.model;
 
+import java.time.LocalDate;
+
 public class Cliente {
+    public int id;
+    public String nome;
+    public String email;
+    public LocalDate dataNascimento;
+
+    public void exibirCliente(){
+        System.out.println("ID: " + id);
+        System.out.println("NOME: " + nome);
+        System.out.println("EMAIL: " + email);
+        System.out.println("DATA NASCII: " + dataNascimento);
+    }
+
 }
+
